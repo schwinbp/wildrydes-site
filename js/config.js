592f55a4defcb085d0c75e1c6ee070ea79603961
schwinbp <schwinbp@airproducts.com>
2021-12-28 10:30:39 -0500

    Cognito changes

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_iIJm64CBb', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 's9aouajvelt0dl9cn4kfko5vf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
